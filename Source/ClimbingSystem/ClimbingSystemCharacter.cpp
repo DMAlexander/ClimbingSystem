f1bf823a3add11591c0acf31ca34b2e920363e9e
Devin Alexander <devinmalexander@gmail.com>
2025-07-31 14:37:46 -0500

    Cross Product

--- Source/ClimbingSystem/ClimbingSystemCharacter.cpp
+++ Source/ClimbingSystem/ClimbingSystemCharacter.cpp
@@ -95,11 +95,25 @@ void AClimbingSystemCharacter::SetupPlayerInputComponent(class UInputComponent *
 	}
 
 }
-
 void AClimbingSystemCharacter::Move(const FInputActionValue& Value)
 {
+	if(!CustomMovementComponent) return;
+
+	if(CustomMovementComponent->IsClimbing())
+	{
+		HandleClimbMovementInput(Value);
+	}
+	else
+	{
+		HandleGroundMovementInput(Value);
+	}
+
+}
+
+void AClimbingSystemCharacter::HandleGroundMovementInput(const FInputActionValue & Value)
+{
 	// input is a Vector2D
-	FVector2D MovementVector = Value.Get<FVector2D>();
+	const FVector2D MovementVector = Value.Get<FVector2D>();
 
 	if (Controller != nullptr)
 	{
@@ -117,6 +131,26 @@ void AClimbingSystemCharacter::Move(const FInputActionValue& Value)
 		AddMovementInput(ForwardDirection, MovementVector.Y);
 		AddMovementInput(RightDirection, MovementVector.X);
 	}
+}
+
+void AClimbingSystemCharacter::HandleClimbMovementInput(const FInputActionValue & Value)
+{
+	// input is a vector2D
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FVector ForwardDirection = FVector::CrossProduct(
+		-CustomMovementComponent->GetClimbableSurfaceNormal(),
+		GetActorRightVector()
+	);
+	
+	const FVector RightDirection = FVector::CrossProduct(
+		-CustomMovementComponent->GetClimbableSurfaceNormal(),
+		-GetActorUpVector()
+	);
+
+	// add movement 
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+	AddMovementInput(RightDirection, MovementVector.X);
 }
 
 void AClimbingSystemCharacter::Look(const FInputActionValue& Value)
